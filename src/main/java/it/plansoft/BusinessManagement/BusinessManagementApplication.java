5c577d02ec7a54d193a866e8f59be52dbd174f1c
Leonardo Morganti <leonardo.morganti00@gmail.com>
2021-02-26 10:00:53 +0100

    Inizializzazione progetto
    Creato il progetto e aggiunte le dipendenze necessarie

---
+++ src/main/java/it/plansoft/BusinessManagement/BusinessManagementApplication.java
@@ -0,0 +1,13 @@
+package it.plansoft.BusinessManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessManagementApplication.class, args);
+	}
+
+}
