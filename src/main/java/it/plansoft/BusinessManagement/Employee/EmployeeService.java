8d6de328fe8f957faca7cedbb92758d470c15672
Leonardo Morganti <leonardo.morganti00@gmail.com>
2021-02-26 17:19:20 +0100

    Aggiunte funzionalità

--- src/main/java/it/plansoft/BusinessManagement/Employee/EmployeeService.java
+++ src/main/java/it/plansoft/BusinessManagement/Employee/EmployeeService.java
@@ -4,46 +4,45 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
 	
-	//@Autowired
-	//private EmployeeRepository employeeRepository;
+	@Autowired
+	private EmployeeRepository employeeRepository;
 	
-	public void getAllemployees() {
-		/*
+	public List<Employee> getAllemployees() {
 		List<Employee> employees = new ArrayList<>();
 		employeeRepository.findAll()
 		 .forEach(employees::add);
 		return employees;
-		*/
 	}
 	
-	public void getEmployee(String id) {
-		//void -> Optional<Employee>
-		//return employeeRepository.findById(id);
+	public Optional<Employee> getEmployee(Long id) {
+		return employeeRepository.findById(id);
 	}
 
-	public void addEmployee() {
-		//Employee employee
-		//employeeRepository.save(employee);
+	public void addEmployee(Employee employee) {
+		employeeRepository.save(employee);
 	}
 
-	public void updateEmployee(String id) {
-		//Employee employee
-		//employeeRepository.save(employee);
+	public void updateEmployee(Long id, Employee employee) {
+		employeeRepository.save(employee);
 		
 	}
 
-	public void deleteEmployee(String id) {
-		//employeeRepository.deleteById(id);
+	public void deleteEmployee(Long id) {
+		employeeRepository.deleteById(id);
 	}
 
 	public double getSalary(Long id) {
-		
-		return 0;
+		double baseSalary = employeeRepository.findById(id).get().getPayRoll().getSalary();
+		int promotions = employeeRepository.findById(id).get().getPayRoll().getPromotions();
+		int extraHours = employeeRepository.findById(id).get().getTimeBank().getExtraHours();
+		int dailyHours = employeeRepository.findById(id).get().getTimeBank().getDailyHours();
+		return baseSalary+(promotions)+(extraHours*(baseSalary/30*dailyHours)*1.7);
 	}
 
 	public void promote(Employee employee, Long id) {
