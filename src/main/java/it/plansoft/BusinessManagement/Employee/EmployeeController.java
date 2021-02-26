78276df22741ab1a3abc44664828d94414175a1d
Leonardo Morganti <leonardo.morganti00@gmail.com>
2021-02-26 16:10:39 +0100

    Aggiunti endpoint

--- src/main/java/it/plansoft/BusinessManagement/Employee/EmployeeController.java
+++ src/main/java/it/plansoft/BusinessManagement/Employee/EmployeeController.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,18 +14,16 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.sun.org.slf4j.internal.Logger;
-
-
 @RestController
 public class EmployeeController {
 	
 	//private EmployeeService employeeService
 	
-	//private Logger log = 
+	private Logger log = LoggerFactory.getLogger(EmployeeController.class);
 	
 	@GetMapping("/employees")
 	public List<String> getAllEmployees() {
+		log.info("getAllEmployees");
 		return Arrays.asList("ciao", "doge");
 		
 		//employeeService.getAllEmployees();
@@ -31,24 +31,58 @@ public class EmployeeController {
 	
 	@GetMapping("employees/{id}")
 	public void getEmployee(@PathVariable Long id){
+		log.info("getEmployee");
 		//void -> Optional<Employee>
 		//employeeService.getEmployee(id);
 	}
 	
 	@PostMapping("/employees")
 	public void addEmployee(@RequestBody Employee employee) {
+		log.info("addEmployee");
 		//employeeService.addEmployee(employee);
 	}
 	
 	@DeleteMapping("/employees/{id}")
 	public void deleteEmployee(@PathVariable Long id) {
+		log.info("deleteEmployee");
 		//employeeService.deleteEmployee(id);
 	}
 	
 	
 	@PutMapping("/employees/{id}")
 	public void updateEmployee(@RequestBody Employee employee, @PathVariable Long id) {
+		log.info("updateEmployee");
 		//void -> Optional<Employee>
 		//employeeService.updateEmployee(employee, id);
+	}
+	
+	@GetMapping("employees/{id}/salary")
+	public double getSalary(@PathVariable Long id) {
+		return employeeService.getSalary(id);
+	}
+	
+	@PutMapping("employees/{id}/promotion")
+	public void promote(@RequestBody Employee employee, @PathVariable Long id) {
+		employeeService.promote(employee, id);
+	}
+	
+	@PutMapping("employees/{id}/position")
+	public void changePosition(@RequestBody Employee employee, @PathVariable Long id) {
+		employeeService.changePosition(employee, id);
+	}
+	
+	@PutMapping("employees/{id}/salary")
+	public void changeSalary(@RequestBody Employee employee, @PathVariable Long id) {
+		employeeService.changeSalary(employee, id);
+	}
+	
+	@PutMapping("employees/{id}/dailyhours")
+	public void changeDailyHours(@RequestBody Employee employee, @PathVariable Long id) {
+		employeeService.changeDailyHours(employee, id);
+	}
+	
+	@PutMapping("employees/{id}/actualHours")
+	public void setActualHours(@RequestBody Employee employee, @PathVariable Long id) {
+		employeeService.setActualHours(employee, id);
 	}
 }
